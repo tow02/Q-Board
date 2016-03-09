96f80fcb29632492ea9c16cc2df1e6dc1d97df80
Paween Theanvanichpant <paween.the@gmail.com>
2016-03-09 16:34:11 +0700

    Add unit_test for UserRole model

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -12,5 +12,20 @@
 require 'rails_helper'
 
 RSpec.describe UserRole, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user1_role1 = UserRole.new(user_id: 1, role_id: 1)
+    @user1_role_2 = UserRole.new(user_id: 1, role_id: 1)
+  end
+
+  it "user_id and role_id should be unique together" do
+    @user1_role1.save
+    expect(@user1_role_2.save).to be(false)
+  end
+
+  it "create user's role successfully" do
+    @user1_role1.save
+    @user1_role_2.role_id = 2
+    expect(@user1_role_2.save).to be(true)
+  end
 end
